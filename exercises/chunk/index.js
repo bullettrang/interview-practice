2a01a29169eadcb8cfcf1fd3c5eab91d6a72c01d
brian trang <trangb@oregonstate.edu>
2018-12-04 17:27:59 -0800

    solved chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,35 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const output = [];
+    let idx=0;
+    
+        while(idx < array.length){
+            output.push(array.slice(idx,idx+size));
+            idx=idx+size;
+            
+        }
+    return output;
+    
+}
+
+//first attempt
+// function chunk(array, size) {
+//     const increment =size;
+//      const output = [];
+//      for(let i =0;i<array.length;i+=increment){
+//          let subArr=[];
+//          let count=0;
+//          let j=i;
+//          while(count<size && j<array.length){
+//              subArr.push(array[j++]);
+//              count++;
+//          }
+//          output.push(subArr);
+//      }
+//      return output;
+//  }
+
 
 module.exports = chunk;
